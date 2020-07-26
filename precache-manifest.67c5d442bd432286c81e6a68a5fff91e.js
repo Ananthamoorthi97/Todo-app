ef86e72df6004b67b0269726a944d0d9cb6c0181
Ananthamoorthi <ananthamoorthi.s.l@gmail.com>
2020-07-26 21:11:03 +0530

    Updates

---
+++ precache-manifest.67c5d442bd432286c81e6a68a5fff91e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d368dfe5246e7cc5d64e67e8b5b579ac",
+    "url": "/Todo-app/index.html"
+  },
+  {
+    "revision": "b3e0fe95b11665fe9a5a",
+    "url": "/Todo-app/static/css/main.48097d5a.chunk.css"
+  },
+  {
+    "revision": "f270abe14f9603b1509d",
+    "url": "/Todo-app/static/js/2.1e90d6a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-app/static/js/2.1e90d6a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3e0fe95b11665fe9a5a",
+    "url": "/Todo-app/static/js/main.1ba27a10.chunk.js"
+  },
+  {
+    "revision": "9f48de200ba6786cff8a",
+    "url": "/Todo-app/static/js/runtime-main.e9bd9999.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Todo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
